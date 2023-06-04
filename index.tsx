2a5b37874df4bf596eade78acd435f06c27fbfdc
shivangkatiyar <shivangkatiyar2001@gmail.com>
2023-06-04 23:22:12 +0530

    second commit

--- index.tsx
+++ index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import App from "./src/App";
+import reportWebVitals from "./src/reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
-import { store } from "./components/middleware/store";
+import { store } from "./src/components/middleware/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
